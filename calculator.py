821b593929393ec568032c1a456903955f7f6cf7
Ana Corina Ruiz <anacorinaruiz@MacBook-Air-128.local>
2025-11-12 15:18:29 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -7,6 +7,12 @@ One function per operation, in order.
 
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError('Invalid input')
+    return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 def add(a, b): 
     return a + b
 def sub(a, b):
