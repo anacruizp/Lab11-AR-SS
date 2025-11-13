e3fd045dc54191f4df8e6e7e2014fc0d774c385d
anacruizp <ruiz.ac@ufl.edu>
2025-11-12 21:35:40 -0500

    Fix division order in div function

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ def mul(a,b):
 def div(a,b):
     if a == 0:
         raise ZeroDivisonError("division by zero")
-    return b/a
+    return a/b
 def logarithm(a,b):
      if a <= 0 or a ==1 or b <= 0:
          raise ValueError("invalid input")
