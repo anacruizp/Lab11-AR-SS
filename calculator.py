a2ca30af8e3287ab6b5b11b8778eb1f971ba9c6a
anacruizp <ruiz.ac@ufl.edu>
2025-11-12 21:37:39 -0500

    Fix logarithm function input validation

--- calculator.py
+++ calculator.py
@@ -19,7 +19,7 @@ def div(a,b):
         raise ZeroDivisonError("division by zero")
     return a/b
 def logarithm(a,b):
-     if a <= 0 or a ==1 or b <= 0:
+     if a <= 0 or b == 1 or b <= 0:
          raise ValueError("invalid input")
      return math.log(b,a)
 def exp(a,b):
